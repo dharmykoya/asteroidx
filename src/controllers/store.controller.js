d89951ec28771d65e92d160a867618891f0df093
Damilola Adekoya <dharmykoya38@gmail.com>
2020-11-18 11:08:50 +0100

    feature(single-store):get a single store with id

--- src/controllers/store.controller.js
+++ src/controllers/store.controller.js
@@ -1,4 +1,4 @@
-import { createStoreService } from "../services/store.service";
+import { createStoreService, getStoreService } from "../services/store.service";
 
 export default {
   async createStore(request, response) {
@@ -19,6 +19,29 @@ export default {
       return response.status(500).json({
         status: "fail",
         message: error
+      });
+    }
+  },
+  async getStore(request, response) {
+    try {
+      const {storeId} = request.params
+      const data = await getStoreService(storeId);
+
+      if (!data) {
+        return response.status(200).json({
+          status: "success",
+          message: "store not found"
+        });
+      }
+
+      return response.status(200).json({
+        status: "success",
+        data
+      });
+    } catch (error) {
+      return response.status(500).json({
+        status: "fail",
+        message: error.message,
       });
     }
   }
--- src/services/store.service.js
+++ src/services/store.service.js
@@ -4,3 +4,8 @@ import model from '../database/models';
 export const createStoreService = async data => {
   return await model.Store.create({ name: data.name, url: data.url });
 };
+
+export const getStoreService = async storeId => {
+  const store = await model.Store.findByPk(storeId);
+  return store;
+};
